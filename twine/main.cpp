1355f9b4034b43520df85c0a220a654e17c22245
user.name <vianmart@yandex.ru>
2025-10-10 15:30:24 +0300

    slight refactor

--- twine/main.cpp
+++ twine/main.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <ostream>
+
 #include "twine.hpp"
 
 int main() {
     using namespace HomeworkTwine;
-    //std::cout << 0 << std::is_convertible_v<Twine, TwineChild> << '\n';
-    //std::cout << "AB\n";
-    //std::cout << HomeworkTwine::Twine("ab", "oba") << '\n';
-    //HomeworkTwine::Twine("ab", "oba", "x");
-    //Twine(Twine("hello", ", "), "world!").dump_impl(std::cout);
+    // std::cout << 0 << std::is_convertible_v<Twine, TwineChild> << '\n';
+    // std::cout << "AB\n";
+    // std::cout << HomeworkTwine::Twine("ab", "oba") << '\n';
+    // HomeworkTwine::Twine("ab", "oba", "x");
+    // Twine(Twine("hello", ", "), "world!").dump_impl(std::cout);
     std::cout << Twine("hello", ", ") + Twine("world", "!") << std::endl;
-    //std::cout << Twine("hello", ", ", "world!") << std::endl;
+    // std::cout << Twine("hello", ", ", "world!") << std::endl;
+    //
+
+    std::cout << Twine("ABOBA!!", HomeworkTwine::Twine::EmptyTwineChild());
+    std::cout << Twine("Aboba", "boba") << std::endl;
+    //return 0;
+    std::cout << Twine("hello", ", ") + Twine("world!") + Twine("ABIBAIBIAIB") +
+                     Twine("\n\n\n\n") + Twine("abiba", "biboba\n")
+              << std::endl;
+    std::cout << Twine(Twine(Twine(Twine("hello", ", ") + Twine("world!")),
+                             Twine(Twine("ABIBAIBIAIB"), Twine("\n\n\n\n"))),
+                       Twine("abiba", "biboba\n"))
+              << std::endl;
+    std::cout << Twine(Twine("hello", ", "), Twine("world!")) << std::endl;
+
+    // std::cout << Twine::from_several("hello", ", ", "world", "!") << std::endl;
 
     std::cout << "AB\n";
-    //HomeworkTwine::Twine("hello", ", ", "world").dump_impl(std::cout);
-    //std::cout << "AB\n";
-    //std::cout << HomeworkTwine::Twine("hello", ", ", "world") << '\n';
+    // HomeworkTwine::Twine("hello", ", ", "world").dump_impl(std::cout);
+    // std::cout << "AB\n";
+    // std::cout << HomeworkTwine::Twine("hello", ", ", "world") << '\n';
 }
-
--- twine/twine.hpp
+++ twine/twine.hpp
@@ -1,92 +1,41 @@
 #pragma once
 
-#include <concepts>
 #include <iostream>
 #include <ostream>
-#include <stdexcept>
 #include <string_view>
 #include <type_traits>
 #include <variant>
 
+#include "overloaded.hpp"
+
 namespace HomeworkTwine {
 
-template <class... Ts>
-struct overloaded : Ts... {
-    using Ts::operator()...;
-};
-
-template <class... Ts>
-overloaded(Ts...) -> overloaded<Ts...>;
-
-struct Dumper {
-    Dumper(const char *str) { std::cout << str << '\n'; }
-    Dumper(int x) { std::cout << x << '\n'; }
-};
-
-class EmptyTwineChild {};
-
-struct Twine;
-inline std::ostream &operator<<(std::ostream &out, const Twine &rhs);
-
-typedef std::variant<const char *, std::string_view, const Twine *, EmptyTwineChild>
-    TwineChildBase;
-
-struct TwineChild : TwineChildBase {
-    template <typename Arg>
-    explicit TwineChild(const Arg &arg) : TwineChildBase(arg) {
-        std::cout << "templated child: " << typeid(arg).name() << ' ' << arg << "\n";
-    }
-
-    explicit TwineChild(const Twine &arg) : TwineChildBase(&arg) {
-        std::cout << "const twine & child\n";
-    }
-
-    std::ostream &dump_impl(std::ostream &out) const;
-
-    template <typename T>
-    bool is_type() const {
-        return std::holds_alternative<T>(*this);
-    }
-};
-
-inline std::ostream &operator<<(std::ostream &out, const TwineChild &rhs) {
-    auto functor = [&](auto &arg) -> std::ostream & {
-        using T = std::decay_t<decltype(arg)>;
-        if constexpr (std::is_same_v<T, EmptyTwineChild>)
-            return out;
-        else if constexpr (std::is_same_v<T, const Twine *>)
-            return out << *arg;
-        else
-            return out << arg;
-    };
-
-    return std::visit(functor, rhs);
-}
-
 struct Twine {
-    Dumper dumper;
+    class EmptyTwineChild {};
+
+    using TwineChildBase =
+        std::variant<const char *, std::string_view, const Twine *, EmptyTwineChild>;
+
+    struct TwineChild : TwineChildBase {
+        template <typename Arg>
+        explicit TwineChild(const Arg &arg) : TwineChildBase(arg) {}
+
+        explicit TwineChild(const Twine &arg) : TwineChildBase(&arg) {}
+
+        std::ostream &dump_impl(std::ostream &out) const;
+
+        template <typename T>
+        bool is_type() const {
+            return std::holds_alternative<T>(*this);
+        }
+    };
 
     TwineChild lhs{EmptyTwineChild()};
     TwineChild rhs{EmptyTwineChild()};
 
-    // template <std::convertible_to<TwineChild> LT = EmptyChild,
-    //           std::convertible_to<TwineChild> RT = EmptyChild>
     template <typename LT = EmptyTwineChild, typename RT = EmptyTwineChild>
     explicit Twine(const LT &lhs_ = EmptyTwineChild(), const RT &rhs_ = EmptyTwineChild())
-        : dumper("constr of 2"), lhs(lhs_), rhs(rhs_) {
-        // std::cout << "!constr of " << 2 << '\n';
-        std::cout << "lhs_: " << typeid(lhs_).name() << ' ' << lhs_ << '\n';
-        std::cout << "rhs_: " << typeid(rhs_).name() << ' ' << rhs_ << '\n';
-        dump_impl(std::cout) << '\n';
-    }
-
-    template <typename First, typename... Args>
-        requires(sizeof...(Args) > 1)
-    explicit Twine(const First &first, const Args &...args)
-        : Twine(first, Twine(args...)) {
-        std::cout << "constr of many: " << sizeof...(Args) + 1 << '\n';
-        dump_impl(std::cout) << '\n';
-    }
+        : lhs(lhs_), rhs(rhs_) {}
 
     std::ostream &dump_impl(std::ostream &out) const {
         lhs.dump_impl(out << "lhs: {") << '}';
@@ -96,27 +45,40 @@ struct Twine {
     Twine operator+(const Twine &rhs) { return Twine(*this, rhs); }
 };
 
-inline std::ostream &TwineChild::dump_impl(std::ostream &out) const {
+inline std::ostream &Twine::TwineChild::dump_impl(std::ostream &out) const {
     return std::visit(
-        overloaded{[&](const char *val) -> std::ostream & {
-                       return out << "char*: " << (void *)val << " [" << val << "] ";
-                   },
-                   [&](std::string_view val) -> std::ostream & {
-                       return out << "string_view: [" << val << "] ";
-                   },
-                   [&](const Twine *val) -> std::ostream & {
-                       return val->dump_impl(out << "twine: [" << &val << "\n") << "\n] ";
-                   },
-                   [&](EmptyTwineChild) -> std::ostream & { return out << "empty: []"; }},
+        HomeworkCommon::overloaded{
+            [&](const char *val) -> std::ostream & {
+                return out << "char*: " << (void *)val << " [" << val << "] ";
+            },
+            [&](std::string_view val) -> std::ostream & {
+                return out << "string_view: [" << val << "] ";
+            },
+            [&](const Twine *val) -> std::ostream & {
+                return val->dump_impl(out << "twine: [" << &val << "\n") << "\n] ";
+            },
+            [&](EmptyTwineChild) -> std::ostream & { return out << "empty: []"; }},
         *this);
 }
 
+std::ostream &operator<<(std::ostream &out, const Twine::TwineChild &rhs);
+
 inline std::ostream &operator<<(std::ostream &out, const Twine &rhs) {
-    #ifdef DEBUG
-    std::cout << "oper<< twine\n";
-    rhs.dump_impl(std::cout) << '\n';
-    #endif
     return out << rhs.lhs << rhs.rhs;
+}
+
+inline std::ostream &operator<<(std::ostream &out, const Twine::TwineChild &rhs) {
+    auto functor = [&](auto &arg) -> std::ostream & {
+        using T = std::decay_t<decltype(arg)>;
+        if constexpr (std::is_same_v<T, Twine::EmptyTwineChild>)
+            return out;
+        else if constexpr (std::is_same_v<T, const Twine *>)
+            return out << *arg;
+        else
+            return out << arg;
+    };
+
+    return std::visit(functor, rhs);
 }
 
 }  // namespace HomeworkTwine
