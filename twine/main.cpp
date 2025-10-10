4f8ad64155a4a066190bab252d0d10de35959476
user.name <vianmart@yandex.ru>
2025-10-10 16:01:08 +0300

    moved main to tests

--- twine/main.cpp
+++ twine/main.cpp
@@ -1,21 +1,31 @@
+//#include <cassert>
 #include <iostream>
 #include <ostream>
 
 #include "twine.hpp"
 
+#define assert(cond)       \
+    do {                   \
+        if (!(cond)) abort(); \
+    } while (0)
+
+void tests() {
+    using namespace HomeworkTwine;
+    assert((Twine("hello", ", ") + Twine("world", "!")).str() == "hello, world!");
+    assert((Twine("ABOBA!!", Twine::EmptyTwineChild())).str() == "ABOBA!!");
+    assert((Twine("Aboba", "boba")).str() == "Abobaboba");
+    assert((Twine("hello", ", ") + Twine("world!") + Twine("ABIBAIBIAIB") +
+            Twine("\n\n\n\n") + Twine("abiba", "biboba\n"))
+               .str() == "hello, world!ABIBAIBIAIB\n\n\n\nabibabiboba\n");
+    assert((Twine(Twine("hello", ", "), Twine("world!"))).str() == "hello, world!");
+    // assert((Twine("ABOBA!!", Twine::EmptyTwineChild())).str() == "ABOBA!!");
+    // assert((Twine("ABOBA!!", Twine::EmptyTwineChild())).str() == "ABOBA!!");
+}
+
 int main() {
     using namespace HomeworkTwine;
-    std::cout << Twine("hello", ", ") + Twine("world", "!") << std::endl;
-
-    std::cout << Twine("ABOBA!!", HomeworkTwine::Twine::EmptyTwineChild());
-    std::cout << Twine("Aboba", "boba") << std::endl;
-    std::cout << Twine("hello", ", ") + Twine("world!") + Twine("ABIBAIBIAIB") +
-                     Twine("\n\n\n\n") + Twine("abiba", "biboba\n")
-              << std::endl;
-    std::cout << Twine(Twine(Twine(Twine("hello", ", ") + Twine("world!")),
-                             Twine(Twine("ABIBAIBIAIB"), Twine("\n\n\n\n"))),
-                       Twine("abiba", "biboba\n"))
-              << std::endl;
-    std::cout << Twine(Twine("hello", ", "), Twine("world!")) << std::endl;
-    std::cout << Twine(Twine("hello", ", "), Twine("world!")).str() << std::endl;
+    tests();
+    std::cout << "tests are successfull!\n";
+    // std::cout << Twine(Twine("hello", ", "), Twine("world!")) << std::endl;
+    // std::cout << Twine(Twine("hello", ", "), Twine("world!")).str() << std::endl;
 }
